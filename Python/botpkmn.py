04aeb201fc2ac14e780200054e46a12d4aa7e2b3
Intervencion <janraven55@gmail.com>
2016-12-16 06:37:29 +0100

    Removed duplicate stuff

--- Python/botpkmn.py
+++ Python/botpkmn.py
@@ -170,12 +170,3 @@ def command_help(m):
 
 bot.skip_pending = True		
 bot.polling(none_stop=True)
-@bot.message_handler(commands=['evs'])
-def command_evs(m):
-	cid = m.chat.id
-	bot.reply_to(m,'*HP*\n*–* `Caterpie` : Ruta 1. 1 EV de HP\n*–* `Makuhita` : Ruta 2. 1 EV de HP\n\n*Ataque*\n*–* `Pikipek` : Ruta 1. 1 EV de Ataque\n*–* `Yangoos` : Ruta 1, Ruta 2 (Por el día). 1 EV de Ataque\n*–* `Mankey` : Ruta 3. 1 EV de Ataque\n\n*Defensa*\n*–* `Roggenrola` : Colina Dequilate. 1 EV de Defensa\n*–* `Cubone` : Área Volcánica Wela. 1 EV de Defensa\n*–* `Geodude` : Ruta 12. 1 EV de Defensa\n*–* `Torkoal` : Ruta 12. 2 EVs de Defensa\n\n*Ataque Especial*\n*–* `Magnemite` : Escuela de Entrenadores (Ruta 1). 1 EV de Ataque Especial\n*–* `Oricorio` : Jardines de Melemele. 2 EVs de Ataque Especial\n\n*Defensa Especial*\n*–* `Tentacool` : Mar de Melemele (Surf). 1 EV de Defensa Especial\n*–* `Drowzee` : Ruta 2. 1 EV de Defensa Especial\n\n*Velocidad*\n*–* `Wingull` : Ruta 1, Afueras de Akala. 1 EV de Velocidad\n*–* `Rattata de Alola` : Ruta 1, Ruta 2 (Por la noche). 1 EV de Velocidad\n*–* `Meowth de Alola` : Escuela de Entrenadores (Ruta 1), Ruta 2. 1 EV de Velocidad\n*–* `Spearow` : Ruta 2, Ruta 3. 1 EV de Velocidad', parse_mode="markdown", disable_web_page_preview=True)
-
-@bot.message_handler(commands=['natus'])
-def command_natu(m):
-	cid = m.chat.id
-	bot.reply_to(m,"*Naturalezas*[ ](http://i.imgur.com/IRFr5SG.jpg)", parse_mode="Markdown")
