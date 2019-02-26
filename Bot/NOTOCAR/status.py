d3289ec0352ab5fd85e27138797d28827450895e
Intervención <janraven55@gmail.com>
2019-02-26 23:58:16 +0100

    Fixed status command on private chats

--- Bot/NOTOCAR/status.py
+++ Bot/NOTOCAR/status.py
@@ -4,21 +4,21 @@ from config import *
 def command_status(m):
 	cid = m.chat.id
 	uid = m.from_user.id
-	adms = []
-	admins = bot.get_chat_administrators(m.chat.id)
-	estadisticas = ""
-	c.execute("SELECT * FROM TContador")
-	for i in c:
-		print(f"{i[0]}: {i[1]}")
-		estadisticas += f'`{i[0]}`: {i[1]}\n'
+	adms = []	
 	if cid < 0:
-		for admin in range(len(admins)):
-			r = admins[admin]
-			adms.append(r.user.id)
-			print('\n' + str(admins[admin]))
-			print(adms)
-			bot.send_message(cid, f'Administrador(es): {adms}')
-			bot.send_message(cid, f'Info extensa:  {r}')
-			bot.send_message(cid, f"Desde *2017-06-19 13:00:00*:\n{estadisticas}", parse_mode = "Markdown")
+		admins = bot.get_chat_administrators(m.chat.id)
+		estadisticas = ""
+		c.execute("SELECT * FROM TContador")
+		for i in c:
+			print(f"{i[0]}: {i[1]}")
+			estadisticas += f'`{i[0]}`: {i[1]}\n'
+			for admin in range(len(admins)):
+				r = admins[admin]
+				adms.append(r.user.id)
+				print('\n' + str(admins[admin]))
+				print(adms)
+				bot.send_message(cid, f'Administrador(es): {adms}')
+				bot.send_message(cid, f'Info extensa:  {r}')
+				bot.send_message(cid, f"Desde *2017-06-19 13:00:00*:\n{estadisticas}", parse_mode = "Markdown")
 	else:
 		bot.send_message(cid, 'Este comando solo funciona en grupos.')
